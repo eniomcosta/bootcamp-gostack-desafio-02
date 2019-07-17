7a14941a638afd544b15fe2cb755f9c70e698930
Enio Costa <enio.m.costa@gmail.com>
2019-07-17 20:32:02 -0300

    Created api session strategy using JWT. New route POST /session to create a session to the api.

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConf from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  console.log(authHeader);
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  if (token === 'undefined') {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConf.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
+
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.get('/users', UserController.findAll);
 routes.post('/users', UserController.store);
